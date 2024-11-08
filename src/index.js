376195e42072c6850affe4ccf3f7db9baf7a536b
Éric-Sébastien Lachance <eslachance@gmail.com>
2024-11-08 14:25:13 -0500

    rewrite test

--- src/index.js
+++ src/index.js
@@ -1,67 +1,79 @@
 import { parse as parseCookies, serialize as serializeCookies } from 'cookie';
 
-export const createSessionsMiddleware = async (env, db) => {
-  env.__sessions =
-    env.__sessions ||
-    new Map();
+const destroySessionCookie = serializeCookies('session', '', {
+  httpOnly: true,
+  secure: false,
+  path: '/',
+  sameSite: 'strict',
+  maxAge: 0,
+});
 
-  let cookieJar = [];
+const saveSession = async(env, tableName, dbName) => {
+  console.log('Save Session');
+  console.log(this, env, tableName, dbName);
+  const db = env[dbName];
+  await db.prepare(`INSERT INTO ${tableName} (id, data) VALUES (?, ?)`).run(this.id, JSON.stringify(data));
+};
 
-  return {
-    sessionPreflight: async (request) => {
-      const cookies = parseCookies(request.headers.get('Cookie') || '');
-      if (cookies.session) {
-        if (!env.__sessions.has(cookies.session)) {
-          env.__sessions.set(cookies.session, {}); 
-        }
-        request.session = env.__sessions.get(cookies.session);
-      } else {
-        const sessionID = crypto.randomUUID();
-        env.__sessions.set(sessionID, {});
-        request.session = env.__sessions.get(sessionID);
-        cookieJar.push(
-          serializeCookies('session', sessionID, {
-            httpOnly: true,
-            secure: true,
-            path: '/',
-            sameSite: 'strict',
-            maxAge: 60 * 60 * 24 * 365 * 10,
-          })
-        );
+const destroySession = async (cookieJar, env, tableName, dbName) => {
+  console.log('Destroy Session');
+  console.log(data, env, tableName, dbName);
+  const db = env[dbName];
+  await db.prepare(`DELETE FROM ${tableName} WHERE id = ?`).run(data.id);
+  cookieJar.push(destroySessionCookie);
+};
+
+export const createSessionsMiddleware = (dbName = 'SESSIONS', tableName = 'sessions') => ({
+  sessionPreflight: async (request, env, ctx) => {
+    request.cookieJar = [];
+    const cookies = parseCookies(request.headers.get('Cookie') || '');
+    if (cookies.session) {
+      console.log('Got a session cookie: ', cookies.session);
+      const session = await env[dbName].prepare(`SELECT * FROM ${tableName} WHERE id = ?`).first(cookies.session);
+      if (!session) {
+        console.log('No session found in db, deleting cookie');
+        cookieJar.push(destroySessionCookie);
+        return {};
       }
-    },
-
-    destroy: async (request) => {
-      const cookies = parseCookies(request.headers.get('Cookie') || '');
-      env.__sessions.delete(cookies.session);
+      console.log('returning session from db: ', session)
+      session.save = saveSession.bind(session, env, tableName, dbName);
+      session.destroy = destroySession.bind(null, cookieJar, env, tableName, dbName);
+      request.session = session;
+    } else {
+      console.log('No session cookie found, creating a new one');
+      const sessionID = crypto.randomUUID();
+      console.log('New session ID: ', sessionID);
+      await env[dbName].prepare(`INSERT INTO ${tableName} (id, created_at) VALUES (?, ?)`).run(sessionID, new Date());
+      request.session = {};
       cookieJar.push(
-        serializeCookies('session', '', {
+        serializeCookies('session', sessionID, {
           httpOnly: true,
-          secure: false,
+          secure: true,
           path: '/',
           sameSite: 'strict',
-          maxAge: 0,
+          maxAge: 60 * 60 * 24 * 365 * 10,
         })
       );
-    },
+    }
+  },
 
-    sessionify: async (response) => {
-      if (!response) {
-        throw new Error('No fetch handler responded and no upstream to proxy to specified.');
-      }
-      const { headers, status, body } = response;
-      const existingHeaders = Object.fromEntries(headers);
-      const cookies = cookieJar.join('; ');
-      cookieJar = [];
+  sessionify: async (response, request) => {
+    if (!response) {
+      throw new Error('No fetch handler responded and no upstream to proxy to specified.');
+    }
+    await request.session.save();
+    const { headers, status, body } = response;
+    const existingHeaders = Object.fromEntries(headers);
+    const cookies = cookieJar.join('; ');
+    cookieJar = [];
 
-      return new Response(body, {
-        status,
-        headers: {
-          ...existingHeaders,
-          'set-cookie': cookies,
-          'content-type': headers.get('content-type'),
-        },
-      });
-    },
-  };
-};
+    return new Response(body, {
+      status,
+      headers: {
+        ...existingHeaders,
+        'set-cookie': cookies,
+        'content-type': headers.get('content-type'),
+      },
+    });
+  },
+});
