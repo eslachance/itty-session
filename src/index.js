c2ae4c49dddb9fb963061bd626b30883110ab90b
Éric-Sébastien Lachance <eslachance@gmail.com>
2024-11-09 13:14:07 -0500

    Move to temporary D1-only binding for Cloudflare

--- src/index.js
+++ src/index.js
@@ -1,77 +1,93 @@
 import { parse as parseCookies, serialize as serializeCookies } from 'cookie';
 
-const destroySessionCookie = serializeCookies('session', '', {
-  httpOnly: true,
-  secure: false,
-  path: '/',
-  sameSite: 'strict',
-  maxAge: 0,
-});
+const log = (doLog, level, ...message) => {
+  if(Array.isArray(level)) {
+    message = level;
+    level = 'log';
+  }
+  if(doLog) {
+    console[level](...message);
+  }
+}
 
-const saveSession = async(env, tableName, dbName) => {
-  console.log('Save Session');
-  console.log(this, env, tableName, dbName);
-  const db = env[dbName];
-  await db.prepare(`INSERT INTO ${tableName} (id, data) VALUES (?, ?)`).run(this.id, JSON.stringify(data));
-};
-
-const destroySession = async (cookieJar, env, tableName, dbName) => {
-  console.log('Destroy Session');
-  console.log(data, env, tableName, dbName);
-  const db = env[dbName];
-  await db.prepare(`DELETE FROM ${tableName} WHERE id = ?`).run(data.id);
-  cookieJar.push(destroySessionCookie);
-};
-
-export const createSessionsMiddleware = (dbName = 'SESSIONS', tableName = 'sessions') => ({
-  sessionPreflight: async (request, env, ctx) => {
+export const createSessionsMiddleware = ({
+  dbName = 'SESSIONS',
+  tableName = 'sessions',
+  logging = false,
+} = {}) => ({
+  sessionPreflight: async (request, env) => {
     request.cookieJar = [];
-    const cookies = parseCookies(request.headers.get('Cookie') || '');
-    if (cookies.session) {
-      console.log('Got a session cookie: ', cookies.session);
-      const session = await env[dbName].prepare(`SELECT * FROM ${tableName} WHERE id = ?`).first(cookies.session);
-      if (!session) {
-        console.log('No session found in db, deleting cookie');
-        cookieJar.push(destroySessionCookie);
-        return {};
+    try {
+      const cookies = parseCookies(request.headers.get('Cookie') || '');
+      const sessionData = await env[dbName].prepare(`SELECT * FROM ${tableName} WHERE sid = ?`).bind(cookies?.session).first();
+      log(logging, 'log', `Existing sessionData stored in DB for ${cookies?.session} is: `, JSON.parse(sessionData?.data));
+      request.session = sessionData?.data ? JSON.parse(sessionData?.data) : null;
+      
+      if (!request.session) {
+        const sessionID = crypto.randomUUID();
+        log(logging, 'log', `Creating new session with ID ${sessionID}`);
+        request.session = {};
+        const maxAge = 60 * 60 * 24 * 365 * 10;
+        await env[dbName].prepare(`INSERT INTO ${tableName} (sid, data, expiry) VALUES (?, ?, ?)`).bind(sessionID, JSON.stringify({}), Date.now() + maxAge).run();
+        request.cookieJar.push(
+          serializeCookies('session', sessionID, {
+            httpOnly: true,
+            secure: true,
+            path: '/',
+            sameSite: 'strict',
+            maxAge,
+          })
+        );
       }
-      console.log('returning session from db: ', session)
-      session.save = saveSession.bind(session, env, tableName, dbName);
-      session.destroy = destroySession.bind(null, cookieJar, env, tableName, dbName);
-      request.session = session;
-    } else {
-      console.log('No session cookie found, creating a new one');
-      const sessionID = crypto.randomUUID();
-      console.log('New session ID: ', sessionID);
-      await env[dbName].prepare(`INSERT INTO ${tableName} (id, created_at) VALUES (?, ?)`).run(sessionID, new Date());
-      request.session = {};
-      cookieJar.push(
-        serializeCookies('session', sessionID, {
-          httpOnly: true,
-          secure: true,
-          path: '/',
-          sameSite: 'strict',
-          maxAge: 60 * 60 * 24 * 365 * 10,
-        })
-      );
-    }
-  },
+      
+      request.session.destroy = async () => {
+        const cookies = parseCookies(request.headers.get('Cookie') || '');
+        log(logging, 'log', `Destroying session with ID ${cookies?.session}`);
+        try {
+          await env[dbName].prepare(`DELETE FROM ${tableName} WHERE sid = ?`).bind(cookies?.session).run();
+        } catch (error) {
+          log(logging, 'error', 'Error while destroying session data: ', error);
+        }
 
-  sessionify: async (response, request) => {
+        request.cookieJar.push(
+          serializeCookies('session', '', {
+            httpOnly: true,
+            secure: false,
+            path: '/',
+            sameSite: 'strict',
+            maxAge: 0,
+          })
+        );
+      }
+    } catch(error) {
+      log(logging, 'error', 'Error while creating or reading session data: ', error);
+    }
+    
+  },    
+
+  sessionify: async (response, request, env, ctx) => {
     if (!response) {
       throw new Error('No fetch handler responded and no upstream to proxy to specified.');
     }
-    await request.session.save();
+    const cookies = parseCookies(request.headers.get('Cookie') || '');
+    
     const { headers, status, body } = response;
     const existingHeaders = Object.fromEntries(headers);
-    const cookies = cookieJar.join('; ');
-    cookieJar = [];
+    const responseCookies = request.cookieJar.join('; ');
+    
+    delete request.session.destroy;
+    log(logging, 'log', `Updating session data for session ID ${cookies?.session} with data: `, request.session);
+    try {
+      await env[dbName].prepare(`UPDATE ${tableName} SET data = ? WHERE sid = ?`).bind(JSON.stringify(request.session), cookies?.session).run();
+    } catch (error) {
+      log(logging, 'error', 'Error while updating session data: ', error);
+    }
 
     return new Response(body, {
       status,
       headers: {
         ...existingHeaders,
-        'set-cookie': cookies,
+        'set-cookie': responseCookies,
         'content-type': headers.get('content-type'),
       },
     });
